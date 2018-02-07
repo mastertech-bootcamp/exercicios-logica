34f92ae366327077bae322d511db3dd626fd9993
Bruno Pimentel <brunoapimentel@gmail.com>
2018-02-07 09:14:07 -0200

    Ajustes em exercícios

---
+++ parte03/3-encaixe.js
@@ -0,0 +1,15 @@
+/**
+* Considere uma loja que vende pen drives em atacado. A loja faz o envio
+* pelos correios em embalagens pré-definidas que cabem uma quantidade
+* específica de pen drives.
+*
+* Dado o tamanho das caixas, escolha a menor quantidade de caixas para
+* enviar um lote de pen drives, evitando a sobra de espaço nas caixas.
+* 
+* Ex: Pedido = 179
+*     Caixas = 50, 50, 40, 40  
+*/
+
+let pedido = 179;
+
+let caixasDisponiveis = [5, 15, 40, 50];
